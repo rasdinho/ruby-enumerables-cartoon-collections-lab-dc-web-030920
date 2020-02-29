f08a155629d30e341220ebc1492972fef160aaed
Ras azzab <ras.dinho7@gmail.com>
2020-02-29 04:57:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,16 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index do |x,y|
+    name = x
+    number = y+1
+    puts "#{number}. #{name}"
+  end
 end
-
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map do |x|
+    x.capitalize << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
