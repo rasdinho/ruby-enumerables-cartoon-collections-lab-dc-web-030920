f1f709718faf4fd007351b10a3777fda4f4d938d
Ras azzab <ras.dinho7@gmail.com>
2020-02-29 04:59:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,18 +13,4 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-  calls.any? do |x|
-    x.length > 4
-  end
-  long_planeteer_calls
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-   strings.find do |x|
-    cheese_types.include?(x)
-  end
-end
