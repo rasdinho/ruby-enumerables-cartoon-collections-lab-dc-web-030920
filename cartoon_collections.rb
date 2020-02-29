1fed5c02c92d1b42f756f2b3c0af5b91170df15e
Ras azzab <ras.dinho7@gmail.com>
2020-02-29 04:58:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,10 +15,13 @@ end
 
 def long_planeteer_calls# code an argument here
   # Your code here
-  
+  calls.any? do |x|
+    x.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
